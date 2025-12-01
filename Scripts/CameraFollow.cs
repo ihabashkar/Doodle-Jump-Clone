4728d21ecc60a67c8004d73145643c4defaaff5c
Dev_Breezy <74231821+Breezy1214@users.noreply.github.com>
2025-11-30 21:52:52 -0500

    Fix CameraFollow class and update functionality
    Refactor CameraFollow class to follow a target's vertical position.

--- Scripts/CameraFollow.cs
+++ Scripts/CameraFollow.cs
@@ -1,28 +1,17 @@
-using Systems.Collections.Generic;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 
-public class PlayerController : MonoBehavior
+public class CamerFollow : MonoBehaviour
 {
-    public float moveSpeed = 10f;
-    public Rigitbody2D rb;
-
-    private float moveX;
-
-    void Awake()
+    public Transform target;
+    private void LateUpdate()
     {
-        rb = GetComponent<Rigidbody2D>();
-    }
-
-    void Update()
-    {
-        moveX = Input.GetAxis("Horizontal") * moveSpeed;
-    }
-
-    private void FixedUpdate()
-    {
-        Vector2 velocity = rb.velocity;
-        velocity.x = moveX;
-        rb.velocity = velocity;
+        if (target.position.y > transform.position.y)
+        {
+            Vector3 newPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+            transform.position = newPosition;
+        }
     }
 }
